6ed7c15e2fbcba869e6cda90f8b3b6affd5e8003
Kha Võ <duykha392001@gmail.com>
2021-11-26 21:42:17 +0700

    Updates

---
+++ precache-manifest.f416f234bd501bbf2008cadf4c84ce5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbdb4da3e15150fd8f11d533b318ec4a",
+    "url": "/demo-shopping-cart-redux/index.html"
+  },
+  {
+    "revision": "937efbc3490d1967257a",
+    "url": "/demo-shopping-cart-redux/static/css/main.7e8631f7.chunk.css"
+  },
+  {
+    "revision": "b3b7d912f01db1a177c6",
+    "url": "/demo-shopping-cart-redux/static/js/2.6074522a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-shopping-cart-redux/static/js/2.6074522a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "937efbc3490d1967257a",
+    "url": "/demo-shopping-cart-redux/static/js/main.0edeb690.chunk.js"
+  },
+  {
+    "revision": "ab3eb8df853f3dbd98bf",
+    "url": "/demo-shopping-cart-redux/static/js/runtime-main.59d304cd.js"
+  }
+]);
